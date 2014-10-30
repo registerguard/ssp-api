81e9331c3e80d2f6f5079234cb75b8d9f35faac0
Micky Hulse <mickyhulse@gmail.com>
2014-10-29 23:13:15 -0700

    Updated example + a few tweaks.

--- index.php
+++ index.php
@@ -1,51 +1,51 @@
 <?php
 	
-	header('content-type: application/javascript; charset=utf-8');
+	header('content-type: application/json');
 	
-	# Include DirectorAPI class file
-	# and create a new instance of the class
-	# Be sure to have entered your API key and path in the DirectorPHP.php file.
 	include('classes/DirectorPHP.php');
-	$director = new Director('asdfasdfasdfasdfasdfasdfasdfasdfasdfasd', 'asdf.slideshowpro.com');
+	
+	$director = new Director('hosted-XXXXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXX.slideshowpro.com');
+	
+	$data = array();
+	$meta = array();
+	$index = 0;
 	
 	# When your application is live, it is a good idea to enable caching.
 	# You need to provide a string specific to this page and a time limit 
 	# for the cache. Note that in most cases, Director will be able to ping
 	# back to clear the cache for you after a change is made, so don't be 
 	# afraid to set the time limit to a high number.
-	# 
-	// $director->cache->set('unique_cache_name', '+30 minutes');
-
+	
+	//$director->cache->set('election-2014-general', '+30 minutes');
+	
 	# What sizes do we want?
+	$director->format->add(array('name' => 'large', 'width' => '1960', 'height' => '1960', 'crop' => 0, 'quality' => 100, 'sharpening' => 0));
 	
-	$director->format->add(array('name' => 'large', 'width' => '980', 'height' => '980', 'crop' => 0, 'quality' => 100, 'sharpening' => 0));
+	# Make API call using get_album method. Replace '1' with the numerical ID for your album:
+	$album = $director->album->get(456240, array('images_only' => true));
 
-	# Make API call using get_album method. Replace '1' with the numerical ID for your album
-	$album = $director->album->get(ALBUM_ID, array('images_only' => true));
-
-	# Set images variable for easy access
+	# Set images variable for easy access:
 	$contents = $album->contents;
+	//print_r($contents);
 	
-	$ssp_url = '';
-	$ssp_caption = '';
-	$ssp_title = '';
-	$data = array();
-	$new;
-	foreach ($contents as $image): 
-		$ssp_url = $image->large->url;
-		$ssp_caption = $image->caption;
-		$ssp_title = $image->title;
+	foreach ($contents as $image) {
+		
+		$meta[0] = explode(' (', $image->caption);
+		$meta[1] = explode('/', $meta[0][1]);
+		
 		$data[] = array(
-			'url' => $ssp_url,
-			array(
-				'caption' => $ssp_caption,
-				'title' => $ssp_title
-			),
+			'url' => $image->large->url,
+			'caption' => nl2br(trim($meta[0][0])),
+			'credit' => trim($meta[1][0]),
+			'org' => trim(substr($meta[1][1], 0, -1)),
+			'title' => $image->title,
+			'link' => $image->link
 		);
-	endforeach;
-
-	$json = json_encode($data);
-
-	echo $json;
+		
+		$index++;
+		
+	}
 	
-?>
+	echo ((isset($_GET['callback'])) ? $_GET['callback'] . '(' : '');
+	echo json_encode($data);
+	echo ((isset($_GET['callback'])) ? ')' : '');
